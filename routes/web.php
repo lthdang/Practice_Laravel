0015f288398951059c38769406670e81ed00e270
lthdang <lthdang@ninepoints.vn>
2024-03-07 16:34:53 +0700

    - add function login, logout - add dashboard admin, custom layout - add column role for user table

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +14,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::group(['prefix' => 'admin'], function(){
+    Route::group(['middleware' => 'admin.guest'],function(){
+        Route::get('/login', [App\Http\Controllers\admin\AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [App\Http\Controllers\admin\AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+    });
+    Route::group(['middleware' => 'admin.auth'],function(){
+        Route::get('/dashboard', [\App\Http\Controllers\admin\HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [\App\Http\Controllers\admin\HomeController::class, 'logout'])->name('admin.logout');
+    });
 });
